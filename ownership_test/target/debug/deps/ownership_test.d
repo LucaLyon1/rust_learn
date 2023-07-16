df37189c72b2d2941942e6ba7b10797fb7030c5f
the_administrator <savioluca2@gmail.com>
2023-07-16 17:31:02 +0200

    Ownership test

---
+++ ownership_test/target/debug/deps/ownership_test.d
@@ -0,0 +1,5 @@
+C:\Users\lsavi\Documents\developpement\Rust\ownership_test\target\debug\deps\ownership_test.exe: src\main.rs
+
+C:\Users\lsavi\Documents\developpement\Rust\ownership_test\target\debug\deps\ownership_test.d: src\main.rs
+
+src\main.rs:
