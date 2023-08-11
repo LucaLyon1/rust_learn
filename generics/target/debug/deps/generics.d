c3daceeba99a879251d574bf9649b2b86111a81c
the_administrator <savioluca2@gmail.com>
2023-08-11 19:20:12 +0200

    Generic types

---
+++ generics/target/debug/deps/generics.d
@@ -0,0 +1,5 @@
+C:\Users\lsavi\Documents\developpement\Rust\generics\target\debug\deps\generics.exe: src\main.rs
+
+C:\Users\lsavi\Documents\developpement\Rust\generics\target\debug\deps\generics.d: src\main.rs
+
+src\main.rs:
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+C:\Users\lsavi\Documents\developpement\Rust\generics\target\debug\generics.exe: C:\Users\lsavi\Documents\developpement\Rust\generics\src\main.rs
